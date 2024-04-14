d68a35893f1abdb1960dbaebc6bb87328b8cc92f
GENERAL-PRIME <senamborish@gmail.com>
2024-04-14 20:04:09 +0530

    Added responsive footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import "./Styles/App.css";
 
 function App() {
   return (
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,36 @@
 import React from "react";
+import "./Footer.css";
+import fblogo from "../../assets/Images/facebook-svgrepo-com.svg";
+import ytlogo from "../../assets/Images/youtube-168-svgrepo-com.svg";
+import instalogo from "../../assets/Images/instagram-svgrepo-com.svg";
 
-const year = new Date().getFullYear();
-
-function Footer() {
+const Footer = () => {
   return (
-    <footer>
-      <div className="footer"></div>
-      {year} &copy; byvk
+    <footer className="footer">
+      <div className="links">
+        <h3>Quick Links</h3>
+        <a href="">Home</a>
+        <a href="about">About</a>
+        <a href="activities">Activities</a>
+        <a href="contact">Contact</a>
+      </div>
+      <div className="copyright">
+        <p>&copy; {new Date().getFullYear()} Barasat Yoga Vigyan Kendra</p>
+      </div>
+      <div className="social-media">
+        <h3>Follow Us</h3>
+        <a href="https://facebook.com" target="_blank">
+          <img src={fblogo}></img>
+        </a>
+        <a href="https://facebook.com" target="_blank">
+          <img src={instalogo}></img>
+        </a>
+        <a href="https://facebook.com" target="_blank">
+          <img src={ytlogo}></img>
+        </a>
+      </div>
     </footer>
   );
-}
+};
 
 export default Footer;

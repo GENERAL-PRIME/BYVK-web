1a653f07764248d80b5a8ee07a635d72c2ff70cf
GENERAL-PRIME <senamborish@gmail.com>
2024-04-18 14:12:39 +0530

    Carousel Component added (image not showing to be fixed)

---
+++ src/assets/Data/CarouselPic.jsx
@@ -0,0 +1,24 @@
+import carouselimg from "../../assets/Images/abtimg.jpeg";
+
+export const CarouselPicture = [
+  {
+    image: { carouselimg },
+    title: "First Image",
+  },
+  {
+    image: { carouselimg },
+    title: "Second Image",
+  },
+  {
+    image: { carouselimg },
+    title: "Third Image",
+  },
+  {
+    image: { carouselimg },
+    title: "Fourth Image",
+  },
+  {
+    image: { carouselimg },
+    title: "Fifth Image",
+  },
+];
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import LeaveUsAText from "../../components/LeaveUsAText/LeaveUsAText";
 import GetInTouch from "../../components/GetInTouch/GetInTouch";
+import Carousel from "../../components/Carousel/Carousel";
+import { CarouselPicture } from "../../assets/Data/CarouselPic";
 
 function Home() {
   return (
     <div>
+      <Carousel images={CarouselPicture} />
       <h1>Home</h1>
       <p>This is the home page</p>
       <GetInTouch />

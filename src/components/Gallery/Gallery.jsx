a2efc6492031fb05741c8a9c26e05dd4356d5466
GENERAL-PRIME <senamborish@gmail.com>
2024-04-22 09:33:00 +0530

    Gallery minor fix

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Gallery.css";
+import image1 from "../../assets/Images/fff.png";
+import image2 from "../../assets/Images/fff1.png";
+import image3 from "../../assets/Images/fff2.png";
+import image4 from "../../assets/Images/fff3.png";
+import image5 from "../../assets/Images/fff4.png";
 
 function Gallery() {
   return (
@@ -7,18 +12,18 @@ function Gallery() {
       <h1>Gallery</h1>
       <p>This is the gallery page</p>
       <div className="collage">
-        <div className="row">
-          <div className="column">
-            <img src="https://via.placeholder.com/150" alt="placeholder" />
-            <img src="https://via.placeholder.com/150" alt="placeholder" />
-            <img src="https://via.placeholder.com/150" alt="placeholder" />
+        <div className="collage-row">
+          <div className="collage-column">
+            <img src={image1} alt="placeholder" />
+            <img src={image2} alt="placeholder" />
+            <img src={image3} alt="placeholder" />
           </div>
         </div>
-        <div className="row">
-          <div className="column">
-            <img src="https://via.placeholder.com/150" alt="placeholder" />
-            <img src="https://via.placeholder.com/150" alt="placeholder" />
-            <img src="https://via.placeholder.com/150" alt="placeholder" />
+        <div className="collage-row">
+          <div className="collage-column">
+            <img src={image5} alt="placeholder" />
+            <img src={image4} alt="placeholder" />
+            <img src={image5} alt="placeholder" />
           </div>
         </div>
       </div>
